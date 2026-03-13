ba1ec0b7480af27a6c6b1c8df3e6811c084f0ab6
Cauã Soares <casoares016@gmail.com>
2026-03-13 19:26:38 -0300

    first commit

---
+++ src/main/java/com/ocsoares/sifat_sistemas_developer_test/SifatSistemasDeveloperTestApplication.java
@@ -0,0 +1,14 @@
+package com.ocsoares.sifat_sistemas_developer_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SifatSistemasDeveloperTestApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SifatSistemasDeveloperTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ocsoares/sifat_sistemas_developer_test/SifatSistemasDeveloperTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocsoares.sifat_sistemas_developer_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SifatSistemasDeveloperTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

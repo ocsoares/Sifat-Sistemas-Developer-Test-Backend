1afa67c9d44b5afc501830007b950a648d6f3b8e
Cauã Soares <casoares016@gmail.com>
2026-03-13 23:20:52 -0300

    feat: add CMV Service & Controller

--- src/main/java/com/ocsoares/sifat_sistemas_developer_test/controllers/CmvController.java
+++ src/main/java/com/ocsoares/sifat_sistemas_developer_test/controllers/CmvController.java
@@ -1,4 +1,22 @@
 package com.ocsoares.sifat_sistemas_developer_test.controllers;
 
+import com.ocsoares.sifat_sistemas_developer_test.entities.CmvEntity;
+import com.ocsoares.sifat_sistemas_developer_test.services.CmvService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cmv")
+@RequiredArgsConstructor
 public class CmvController {
+    private final CmvService cmvService;
+
+    @GetMapping()
+    public List<CmvEntity> findAll() {
+        return this.cmvService.findAll();
+    }
 }
--- src/main/java/com/ocsoares/sifat_sistemas_developer_test/services/CmvService.java
+++ src/main/java/com/ocsoares/sifat_sistemas_developer_test/services/CmvService.java
@@ -1,4 +1,19 @@
 package com.ocsoares.sifat_sistemas_developer_test.services;
 
+import com.ocsoares.sifat_sistemas_developer_test.entities.CmvEntity;
+import com.ocsoares.sifat_sistemas_developer_test.repositories.CmvRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class CmvService {
+
+    private final CmvRepository cmvRepository;
+
+    public List<CmvEntity> findAll() {
+        return this.cmvRepository.findAll();
+    }
 }

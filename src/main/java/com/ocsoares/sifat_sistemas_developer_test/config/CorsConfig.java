d5cfd3dcaa31e5971c4e3f72e429917b50a98eac
Cauã Soares <casoares016@gmail.com>
2026-03-14 00:36:31 -0300

    feat: add CorsConfig

---
+++ src/main/java/com/ocsoares/sifat_sistemas_developer_test/config/CorsConfig.java
@@ -0,0 +1,14 @@
+package com.ocsoares.sifat_sistemas_developer_test.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**").allowedOrigins("http://localhost:8080").allowedMethods("GET");
+    }
+}

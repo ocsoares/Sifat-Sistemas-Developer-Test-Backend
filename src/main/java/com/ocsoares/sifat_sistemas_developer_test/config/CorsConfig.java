78181e75668aebda75758cb752b19d82aacdc01d
Cauã Soares <casoares016@gmail.com>
2026-03-14 00:49:07 -0300

    refactor: .env file to cors

--- src/main/java/com/ocsoares/sifat_sistemas_developer_test/config/CorsConfig.java
+++ src/main/java/com/ocsoares/sifat_sistemas_developer_test/config/CorsConfig.java
@@ -1,14 +1,17 @@
 package com.ocsoares.sifat_sistemas_developer_test.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+    @Value("${FRONTEND_URL}")
+    private String frontEndURL;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**").allowedOrigins("http://localhost:8080").allowedMethods("GET");
+        registry.addMapping("/api/**").allowedOrigins(frontEndURL).allowedMethods("GET");
     }
 }
